34ddd6dcb4abd364ce737ab5b4a3be3409d1342d
Brandon Diaz <brandon.diaz.as@gmail.com>
2020-04-06 19:41:33 -0500

    Recorrer arreglos

--- array.js
+++ array.js
@@ -5,3 +5,29 @@ var primerFruta = frutas.unshift("Mango");//Agregar al inicio
 var qUltimaFruta = frutas.pop("Uvas");//Quitar ultimo elemento
 var qPrimerFruta = frutas.shift("Uvas");//Quitar primer elemento
 var posicion = frutas.indexOf("Manzana");// Indice
+
+var objetos = [
+    {nombre = "Bici", costo: 3000},
+    {nombre = "TV", costo: 15000},
+    {nombre = "Libro", costo: 250},
+    {nombre = "Audifonos", costo: 5000}
+]
+var objetosFiltrados = objetos.filter(function (objeto){
+    return objeto.costo <= 1000;
+});
+
+var mapObjetos = objetos.map(function(objeto) {
+    return objeto.nombre;
+});
+
+var encuentra = objetos.find(function(objeto){
+    return objeto.nombre === "Bici";
+});
+
+objetos.forEach(function(objeto){
+    console.log(objeto.nombre);
+});
+
+var baratos = objetos.some (function (objeto){
+    return objeto.costo <= 700;
+});
